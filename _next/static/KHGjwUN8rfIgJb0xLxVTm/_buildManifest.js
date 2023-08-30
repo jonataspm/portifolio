aa50c03fddff104c3779571f8bf00a03719f8df4
Jonatas Pinheiro Mendes <jonatas.mendes@fitbank.com.br>
2023-08-30 11:14:02 -0300

    Updates

---
+++ _next/static/KHGjwUN8rfIgJb0xLxVTm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/943-982e0d155ee9f0f8.js","static/css/1cf676e15b8b8e75.css","static/chunks/pages/index-0b8336591699b684.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
