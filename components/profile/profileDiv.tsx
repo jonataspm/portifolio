8efae76a6e69a5884d636ab4857a89750dae1c0e
Jonatas Pinheiro Mendes <jonatas.mendes@fitbank.com.br>
2023-09-18 10:29:40 -0300

    ajustes

--- components/profile/profileDiv.tsx
+++ components/profile/profileDiv.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ProfDiv } from './style';
-import Image from 'next/image';
+
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
@@ -41,16 +41,16 @@ export default function ProfileDiv() {
 
             <div className='icons'>
 
-                <a href="https://www.linkedin.com/in/jonatasp/">
+                <a  href="https://www.linkedin.com/in/jonatasp/" title='likedinIcon'>
                     <LinkedInIcon />
                 </a>
-                <a href="https://github.com/jonataspm">
+                <a href="https://github.com/jonataspm" title='gitIcon'>
                     <GitHubIcon />
                 </a>
-                <a href="https://www.instagram.com/jonatasad/">
+                <a href="https://www.instagram.com/jonatasad/" title='instIcon'>
                     <InstagramIcon />
                 </a>
-                <a href="https://wa.me/558981078108">
+                <a href="https://wa.me/558981078108" title='zapIcon'>
                     <WhatsAppIcon />
                 </a>
 
