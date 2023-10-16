0bee448f56af705afcc73bd14f06621263e91593
Jonatas Pinheiro Mendes <jonatas.mendes@fitbank.com.br>
2023-10-16 16:58:27 -0300

    Criação de Projetos

---
+++ components/header/sections/4-sec/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FourthSectionComp } from './style';
+import { CardKnow } from '../../../Icons/cardKnowledge';
+
+export const FourthSection = () => {
+  return (
+    <FourthSectionComp>
+      <h1>Projects</h1>
+      <div className='projects-content'>
+      </div>
+    </FourthSectionComp>
+  );
+};
---
+++ components/header/sections/4-sec/style.ts
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const FourthSectionComp = styled.section`
+
+.cards-content{
+  display: grid;
+  grid-template-columns: repeat(auto-fill,  100px );
+  grid-gap: 20px;
+  box-sizing: border-box;
+  width: 100%;
+  margin: 10px 0px;
+}
+`
